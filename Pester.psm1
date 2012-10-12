7363d157e472ee4b99730d3fef1a2ae0f6a4588d
Lee Harold <lcharold@hotmail.com>
2012-10-12 17:30:10 -0500

    revert garbled file

--- Pester.psm1
+++ Pester.psm1
@@ -1,92 +1,92 @@
-$sha$#$sha$ $sha$P$sha$e$sha$s$sha$t$sha$e$sha$r$sha$
-$sha$#$sha$ $sha$V$sha$e$sha$r$sha$s$sha$i$sha$o$sha$n$sha$:$sha$ $sha$$$sha$v$sha$e$sha$r$sha$s$sha$i$sha$o$sha$n$sha$$$sha$
-$sha$#$sha$ $sha$C$sha$h$sha$a$sha$n$sha$g$sha$e$sha$s$sha$e$sha$t$sha$:$sha$ $sha$
-$sha$
-$sha$R$sha$e$sha$s$sha$o$sha$l$sha$v$sha$e$sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$$$sha$P$sha$S$sha$S$sha$c$sha$r$sha$i$sha$p$sha$t$sha$R$sha$o$sha$o$sha$t$sha$\$sha$F$sha$u$sha$n$sha$c$sha$t$sha$i$sha$o$sha$n$sha$s$sha$\$sha$*$sha$.$sha$p$sha$s$sha$1$sha$ $sha$|$sha$ $sha$
-$sha$ $sha$ $sha$ $sha$ $sha$?$sha$ $sha${$sha$ $sha$-$sha$n$sha$o$sha$t$sha$ $sha$($sha$$$sha$_$sha$.$sha$P$sha$r$sha$o$sha$v$sha$i$sha$d$sha$e$sha$r$sha$P$sha$a$sha$t$sha$h$sha$.$sha$C$sha$o$sha$n$sha$t$sha$a$sha$i$sha$n$sha$s$sha$($sha$"$sha$.$sha$T$sha$e$sha$s$sha$t$sha$s$sha$.$sha$"$sha$)$sha$)$sha$ $sha$}$sha$ $sha$|$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$%$sha$ $sha${$sha$ $sha$.$sha$ $sha$$$sha$_$sha$.$sha$P$sha$r$sha$o$sha$v$sha$i$sha$d$sha$e$sha$r$sha$P$sha$a$sha$t$sha$h$sha$ $sha$}$sha$
-$sha$
-$sha$f$sha$u$sha$n$sha$c$sha$t$sha$i$sha$o$sha$n$sha$ $sha$I$sha$n$sha$v$sha$o$sha$k$sha$e$sha$-$sha$P$sha$e$sha$s$sha$t$sha$e$sha$r$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$p$sha$a$sha$r$sha$a$sha$m$sha$($sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$P$sha$a$sha$r$sha$a$sha$m$sha$e$sha$t$sha$e$sha$r$sha$($sha$P$sha$o$sha$s$sha$i$sha$t$sha$i$sha$o$sha$n$sha$=$sha$0$sha$,$sha$M$sha$a$sha$n$sha$d$sha$a$sha$t$sha$o$sha$r$sha$y$sha$=$sha$0$sha$)$sha$]$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$s$sha$t$sha$r$sha$i$sha$n$sha$g$sha$]$sha$$$sha$r$sha$e$sha$l$sha$a$sha$t$sha$i$sha$v$sha$e$sha$_$sha$p$sha$a$sha$t$sha$h$sha$ $sha$=$sha$ $sha$"$sha$.$sha$"$sha$,$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$P$sha$a$sha$r$sha$a$sha$m$sha$e$sha$t$sha$e$sha$r$sha$($sha$P$sha$o$sha$s$sha$i$sha$t$sha$i$sha$o$sha$n$sha$=$sha$1$sha$,$sha$M$sha$a$sha$n$sha$d$sha$a$sha$t$sha$o$sha$r$sha$y$sha$=$sha$0$sha$)$sha$]$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$s$sha$t$sha$r$sha$i$sha$n$sha$g$sha$]$sha$$$sha$t$sha$e$sha$s$sha$t$sha$N$sha$a$sha$m$sha$e$sha$ $sha$=$sha$ $sha$$$sha$n$sha$u$sha$l$sha$l$sha$,$sha$ $sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$P$sha$a$sha$r$sha$a$sha$m$sha$e$sha$t$sha$e$sha$r$sha$($sha$P$sha$o$sha$s$sha$i$sha$t$sha$i$sha$o$sha$n$sha$=$sha$2$sha$,$sha$M$sha$a$sha$n$sha$d$sha$a$sha$t$sha$o$sha$r$sha$y$sha$=$sha$0$sha$)$sha$]$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$s$sha$w$sha$i$sha$t$sha$c$sha$h$sha$]$sha$$$sha$E$sha$n$sha$a$sha$b$sha$l$sha$e$sha$E$sha$x$sha$i$sha$t$sha$,$sha$ $sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$P$sha$a$sha$r$sha$a$sha$m$sha$e$sha$t$sha$e$sha$r$sha$($sha$P$sha$o$sha$s$sha$i$sha$t$sha$i$sha$o$sha$n$sha$=$sha$3$sha$,$sha$M$sha$a$sha$n$sha$d$sha$a$sha$t$sha$o$sha$r$sha$y$sha$=$sha$0$sha$)$sha$]$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$[$sha$s$sha$t$sha$r$sha$i$sha$n$sha$g$sha$]$sha$$$sha$O$sha$u$sha$t$sha$p$sha$u$sha$t$sha$X$sha$m$sha$l$sha$ $sha$=$sha$ $sha$'$sha$'$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$)$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$R$sha$e$sha$s$sha$e$sha$t$sha$-$sha$G$sha$l$sha$o$sha$b$sha$a$sha$l$sha$T$sha$e$sha$s$sha$t$sha$R$sha$e$sha$s$sha$u$sha$l$sha$t$sha$s$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$.$sha$ $sha$"$sha$$$sha$P$sha$S$sha$S$sha$c$sha$r$sha$i$sha$p$sha$t$sha$R$sha$o$sha$o$sha$t$sha$\$sha$O$sha$b$sha$j$sha$e$sha$c$sha$t$sha$A$sha$d$sha$a$sha$p$sha$t$sha$a$sha$t$sha$i$sha$o$sha$n$sha$s$sha$\$sha$P$sha$e$sha$s$sha$t$sha$e$sha$r$sha$F$sha$a$sha$i$sha$l$sha$u$sha$r$sha$e$sha$.$sha$p$sha$s$sha$1$sha$"$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$U$sha$p$sha$d$sha$a$sha$t$sha$e$sha$-$sha$T$sha$y$sha$p$sha$e$sha$D$sha$a$sha$t$sha$a$sha$ $sha$-$sha$p$sha$r$sha$e$sha$ $sha$"$sha$$$sha$P$sha$S$sha$S$sha$c$sha$r$sha$i$sha$p$sha$t$sha$R$sha$o$sha$o$sha$t$sha$\$sha$O$sha$b$sha$j$sha$e$sha$c$sha$t$sha$A$sha$d$sha$a$sha$p$sha$t$sha$a$sha$t$sha$i$sha$o$sha$n$sha$s$sha$\$sha$t$sha$y$sha$p$sha$e$sha$s$sha$.$sha$p$sha$s$sha$1$sha$x$sha$m$sha$l$sha$"$sha$ $sha$-$sha$E$sha$r$sha$r$sha$o$sha$r$sha$A$sha$c$sha$t$sha$i$sha$o$sha$n$sha$ $sha$S$sha$i$sha$l$sha$e$sha$n$sha$t$sha$l$sha$y$sha$C$sha$o$sha$n$sha$t$sha$i$sha$n$sha$u$sha$e$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$$$sha$f$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$s$sha$_$sha$p$sha$a$sha$t$sha$h$sha$ $sha$=$sha$ $sha$R$sha$e$sha$s$sha$o$sha$l$sha$v$sha$e$sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$$$sha$r$sha$e$sha$l$sha$a$sha$t$sha$i$sha$v$sha$e$sha$_$sha$p$sha$a$sha$t$sha$h$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$ $sha$"$sha$E$sha$x$sha$e$sha$c$sha$u$sha$t$sha$i$sha$n$sha$g$sha$ $sha$a$sha$l$sha$l$sha$ $sha$t$sha$e$sha$s$sha$t$sha$s$sha$ $sha$i$sha$n$sha$ $sha$$$sha$f$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$s$sha$_$sha$p$sha$a$sha$t$sha$h$sha$"$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$G$sha$e$sha$t$sha$-$sha$C$sha$h$sha$i$sha$l$sha$d$sha$I$sha$t$sha$e$sha$m$sha$ $sha$$$sha$f$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$s$sha$_$sha$p$sha$a$sha$t$sha$h$sha$ $sha$-$sha$I$sha$n$sha$c$sha$l$sha$u$sha$d$sha$e$sha$ $sha$"$sha$*$sha$.$sha$p$sha$s$sha$1$sha$"$sha$ $sha$-$sha$R$sha$e$sha$c$sha$u$sha$r$sha$s$sha$e$sha$ $sha$|$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$?$sha$ $sha${$sha$ $sha$$$sha$_$sha$.$sha$N$sha$a$sha$m$sha$e$sha$ $sha$-$sha$m$sha$a$sha$t$sha$c$sha$h$sha$ $sha$"$sha$\$sha$.$sha$T$sha$e$sha$s$sha$t$sha$s$sha$\$sha$.$sha$"$sha$ $sha$}$sha$ $sha$|$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$%$sha$ $sha${$sha$ $sha$&$sha$ $sha$$$sha$_$sha$.$sha$P$sha$S$sha$P$sha$a$sha$t$sha$h$sha$ $sha$}$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$T$sha$e$sha$s$sha$t$sha$R$sha$e$sha$p$sha$o$sha$r$sha$t$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$i$sha$f$sha$($sha$$$sha$O$sha$u$sha$t$sha$p$sha$u$sha$t$sha$X$sha$m$sha$l$sha$)$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$$$sha$G$sha$l$sha$o$sha$b$sha$a$sha$l$sha$:$sha$M$sha$o$sha$d$sha$u$sha$l$sha$e$sha$P$sha$a$sha$t$sha$h$sha$ $sha$=$sha$ $sha$$$sha$P$sha$S$sha$S$sha$c$sha$r$sha$i$sha$p$sha$t$sha$R$sha$o$sha$o$sha$t$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$N$sha$u$sha$n$sha$i$sha$t$sha$T$sha$e$sha$s$sha$t$sha$R$sha$e$sha$p$sha$o$sha$r$sha$t$sha$ $sha$($sha$G$sha$e$sha$t$sha$-$sha$G$sha$l$sha$o$sha$b$sha$a$sha$l$sha$T$sha$e$sha$s$sha$t$sha$R$sha$e$sha$s$sha$u$sha$l$sha$t$sha$s$sha$)$sha$ $sha$$$sha$O$sha$u$sha$t$sha$p$sha$u$sha$t$sha$X$sha$m$sha$l$sha$ $sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$i$sha$f$sha$ $sha$($sha$$$sha$E$sha$n$sha$a$sha$b$sha$l$sha$e$sha$E$sha$x$sha$i$sha$t$sha$)$sha$ $sha${$sha$ $sha$E$sha$x$sha$i$sha$t$sha$-$sha$W$sha$i$sha$t$sha$h$sha$C$sha$o$sha$d$sha$e$sha$ $sha$}$sha$
-$sha$}$sha$
-$sha$
-$sha$f$sha$u$sha$n$sha$c$sha$t$sha$i$sha$o$sha$n$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$U$sha$s$sha$a$sha$g$sha$e$sha$F$sha$o$sha$r$sha$C$sha$r$sha$e$sha$a$sha$t$sha$e$sha$F$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$"$sha$i$sha$n$sha$v$sha$a$sha$l$sha$i$sha$d$sha$ $sha$u$sha$s$sha$a$sha$g$sha$e$sha$,$sha$ $sha$p$sha$l$sha$e$sha$a$sha$s$sha$e$sha$ $sha$s$sha$p$sha$e$sha$c$sha$i$sha$f$sha$y$sha$ $sha$($sha$p$sha$a$sha$t$sha$h$sha$,$sha$ $sha$n$sha$a$sha$m$sha$e$sha$)$sha$"$sha$ $sha$|$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$"$sha$e$sha$g$sha$:$sha$ $sha$.$sha$\$sha$C$sha$r$sha$e$sha$a$sha$t$sha$e$sha$-$sha$F$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$ $sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$F$sha$o$sha$o$sha$ $sha$-$sha$N$sha$a$sha$m$sha$e$sha$ $sha$B$sha$a$sha$r$sha$"$sha$ $sha$|$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$"$sha$c$sha$r$sha$e$sha$a$sha$t$sha$e$sha$s$sha$ $sha$.$sha$\$sha$F$sha$o$sha$o$sha$\$sha$B$sha$a$sha$r$sha$.$sha$p$sha$s$sha$1$sha$ $sha$a$sha$n$sha$d$sha$ $sha$.$sha$\$sha$F$sha$o$sha$o$sha$.$sha$B$sha$a$sha$r$sha$.$sha$T$sha$e$sha$s$sha$t$sha$s$sha$.$sha$p$sha$s$sha$1$sha$"$sha$ $sha$|$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$
-$sha$}$sha$
-$sha$
-$sha$f$sha$u$sha$n$sha$c$sha$t$sha$i$sha$o$sha$n$sha$ $sha$C$sha$r$sha$e$sha$a$sha$t$sha$e$sha$-$sha$F$sha$i$sha$l$sha$e$sha$($sha$$$sha$f$sha$i$sha$l$sha$e$sha$_$sha$p$sha$a$sha$t$sha$h$sha$,$sha$ $sha$$$sha$c$sha$o$sha$n$sha$t$sha$e$sha$n$sha$t$sha$s$sha$ $sha$=$sha$ $sha$"$sha$"$sha$)$sha$ $sha${$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$i$sha$f$sha$ $sha$($sha$-$sha$n$sha$o$sha$t$sha$ $sha$($sha$T$sha$e$sha$s$sha$t$sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$$$sha$f$sha$i$sha$l$sha$e$sha$_$sha$p$sha$a$sha$t$sha$h$sha$)$sha$)$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$$$sha$c$sha$o$sha$n$sha$t$sha$e$sha$n$sha$t$sha$s$sha$ $sha$|$sha$ $sha$O$sha$u$sha$t$sha$-$sha$F$sha$i$sha$l$sha$e$sha$ $sha$$$sha$f$sha$i$sha$l$sha$e$sha$_$sha$p$sha$a$sha$t$sha$h$sha$ $sha$-$sha$E$sha$n$sha$c$sha$o$sha$d$sha$i$sha$n$sha$g$sha$ $sha$A$sha$S$sha$C$sha$I$sha$I$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$"$sha$C$sha$r$sha$e$sha$a$sha$t$sha$i$sha$n$sha$g$sha$"$sha$ $sha$|$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$ $sha$-$sha$F$sha$o$sha$r$sha$e$sha$ $sha$G$sha$r$sha$e$sha$e$sha$n$sha$ $sha$-$sha$N$sha$o$sha$N$sha$e$sha$w$sha$L$sha$i$sha$n$sha$e$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$ $sha$e$sha$l$sha$s$sha$e$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$"$sha$S$sha$k$sha$i$sha$p$sha$p$sha$i$sha$n$sha$g$sha$"$sha$ $sha$|$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$ $sha$-$sha$F$sha$o$sha$r$sha$e$sha$ $sha$Y$sha$e$sha$l$sha$l$sha$o$sha$w$sha$ $sha$-$sha$N$sha$o$sha$N$sha$e$sha$w$sha$L$sha$i$sha$n$sha$e$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$"$sha$ $sha$=$sha$>$sha$ $sha$$$sha$f$sha$i$sha$l$sha$e$sha$_$sha$p$sha$a$sha$t$sha$h$sha$"$sha$ $sha$|$sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$H$sha$o$sha$s$sha$t$sha$
-$sha$}$sha$
-$sha$
-$sha$f$sha$u$sha$n$sha$c$sha$t$sha$i$sha$o$sha$n$sha$ $sha$N$sha$e$sha$w$sha$-$sha$F$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$($sha$$$sha$p$sha$a$sha$t$sha$h$sha$,$sha$ $sha$$$sha$n$sha$a$sha$m$sha$e$sha$)$sha$ $sha${$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$i$sha$f$sha$ $sha$($sha$[$sha$S$sha$t$sha$r$sha$i$sha$n$sha$g$sha$]$sha$:$sha$:$sha$I$sha$s$sha$N$sha$u$sha$l$sha$l$sha$O$sha$r$sha$E$sha$m$sha$p$sha$t$sha$y$sha$($sha$$$sha$p$sha$a$sha$t$sha$h$sha$)$sha$ $sha$-$sha$o$sha$r$sha$ $sha$[$sha$S$sha$t$sha$r$sha$i$sha$n$sha$g$sha$]$sha$:$sha$:$sha$I$sha$s$sha$N$sha$u$sha$l$sha$l$sha$O$sha$r$sha$E$sha$m$sha$p$sha$t$sha$y$sha$($sha$$$sha$n$sha$a$sha$m$sha$e$sha$)$sha$)$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$W$sha$r$sha$i$sha$t$sha$e$sha$-$sha$U$sha$s$sha$a$sha$g$sha$e$sha$F$sha$o$sha$r$sha$C$sha$r$sha$e$sha$a$sha$t$sha$e$sha$F$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$r$sha$e$sha$t$sha$u$sha$r$sha$n$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$#$sha$ $sha$T$sha$O$sha$D$sha$O$sha$ $sha$c$sha$l$sha$e$sha$a$sha$n$sha$ $sha$u$sha$p$sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$ $sha$c$sha$l$sha$e$sha$a$sha$n$sha$u$sha$p$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$ $sha$=$sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$.$sha$T$sha$r$sha$i$sha$m$sha$S$sha$t$sha$a$sha$r$sha$t$sha$($sha$"$sha$.$sha$"$sha$)$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$ $sha$=$sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$.$sha$T$sha$r$sha$i$sha$m$sha$S$sha$t$sha$a$sha$r$sha$t$sha$($sha$"$sha$\$sha$"$sha$)$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$i$sha$f$sha$ $sha$($sha$-$sha$n$sha$o$sha$t$sha$ $sha$($sha$T$sha$e$sha$s$sha$t$sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$)$sha$)$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$&$sha$ $sha$m$sha$d$sha$ $sha$$$sha$p$sha$a$sha$t$sha$h$sha$ $sha$|$sha$ $sha$O$sha$u$sha$t$sha$-$sha$N$sha$u$sha$l$sha$l$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$$$sha$t$sha$e$sha$s$sha$t$sha$_$sha$c$sha$o$sha$d$sha$e$sha$ $sha$=$sha$ $sha$"$sha$f$sha$u$sha$n$sha$c$sha$t$sha$i$sha$o$sha$n$sha$ $sha$$$sha$n$sha$a$sha$m$sha$e$sha$ $sha${$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$"$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$$$sha$f$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$_$sha$c$sha$o$sha$d$sha$e$sha$ $sha$=$sha$ $sha$"$sha$`$sha$$$sha$h$sha$e$sha$r$sha$e$sha$ $sha$=$sha$ $sha$S$sha$p$sha$l$sha$i$sha$t$sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$-$sha$P$sha$a$sha$r$sha$e$sha$n$sha$t$sha$ $sha$`$sha$$$sha$M$sha$y$sha$I$sha$n$sha$v$sha$o$sha$c$sha$a$sha$t$sha$i$sha$o$sha$n$sha$.$sha$M$sha$y$sha$C$sha$o$sha$m$sha$m$sha$a$sha$n$sha$d$sha$.$sha$P$sha$a$sha$t$sha$h$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$`$sha$$$sha$s$sha$u$sha$t$sha$ $sha$=$sha$ $sha$($sha$S$sha$p$sha$l$sha$i$sha$t$sha$-$sha$P$sha$a$sha$t$sha$h$sha$ $sha$-$sha$L$sha$e$sha$a$sha$f$sha$ $sha$`$sha$$$sha$M$sha$y$sha$I$sha$n$sha$v$sha$o$sha$c$sha$a$sha$t$sha$i$sha$o$sha$n$sha$.$sha$M$sha$y$sha$C$sha$o$sha$m$sha$m$sha$a$sha$n$sha$d$sha$.$sha$P$sha$a$sha$t$sha$h$sha$)$sha$.$sha$R$sha$e$sha$p$sha$l$sha$a$sha$c$sha$e$sha$($sha$`$sha$"$sha$.$sha$T$sha$e$sha$s$sha$t$sha$s$sha$.$sha$`$sha$"$sha$,$sha$ $sha$`$sha$"$sha$.$sha$`$sha$"$sha$)$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$.$sha$ $sha$`$sha$"$sha$`$sha$$$sha$h$sha$e$sha$r$sha$e$sha$\$sha$`$sha$$$sha$s$sha$u$sha$t$sha$`$sha$"$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$D$sha$e$sha$s$sha$c$sha$r$sha$i$sha$b$sha$e$sha$ $sha$`$sha$"$sha$$$sha$n$sha$a$sha$m$sha$e$sha$`$sha$"$sha$ $sha${$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$I$sha$t$sha$ $sha$`$sha$"$sha$d$sha$o$sha$e$sha$s$sha$ $sha$s$sha$o$sha$m$sha$e$sha$t$sha$h$sha$i$sha$n$sha$g$sha$ $sha$u$sha$s$sha$e$sha$f$sha$u$sha$l$sha$`$sha$"$sha$ $sha${$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$`$sha$$$sha$t$sha$r$sha$u$sha$e$sha$.$sha$s$sha$h$sha$o$sha$u$sha$l$sha$d$sha$.$sha$b$sha$e$sha$($sha$`$sha$$$sha$f$sha$a$sha$l$sha$s$sha$e$sha$)$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$ $sha$}$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$}$sha$"$sha$
-$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$C$sha$r$sha$e$sha$a$sha$t$sha$e$sha$-$sha$F$sha$i$sha$l$sha$e$sha$ $sha$"$sha$$$sha$p$sha$a$sha$t$sha$h$sha$\$sha$$$sha$n$sha$a$sha$m$sha$e$sha$.$sha$p$sha$s$sha$1$sha$"$sha$ $sha$$$sha$t$sha$e$sha$s$sha$t$sha$_$sha$c$sha$o$sha$d$sha$e$sha$
-$sha$ $sha$ $sha$ $sha$ $sha$C$sha$r$sha$e$sha$a$sha$t$sha$e$sha$-$sha$F$sha$i$sha$l$sha$e$sha$ $sha$"$sha$$$sha$p$sha$a$sha$t$sha$h$sha$\$sha$$$sha$n$sha$a$sha$m$sha$e$sha$.$sha$T$sha$e$sha$s$sha$t$sha$s$sha$.$sha$p$sha$s$sha$1$sha$"$sha$ $sha$$$sha$f$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$_$sha$c$sha$o$sha$d$sha$e$sha$
-$sha$}$sha$
-$sha$
-$sha$E$sha$x$sha$p$sha$o$sha$r$sha$t$sha$-$sha$M$sha$o$sha$d$sha$u$sha$l$sha$e$sha$M$sha$e$sha$m$sha$b$sha$e$sha$r$sha$ $sha$D$sha$e$sha$s$sha$c$sha$r$sha$i$sha$b$sha$e$sha$,$sha$ $sha$C$sha$o$sha$n$sha$t$sha$e$sha$x$sha$t$sha$,$sha$ $sha$I$sha$t$sha$,$sha$ $sha$I$sha$n$sha$
-$sha$E$sha$x$sha$p$sha$o$sha$r$sha$t$sha$-$sha$M$sha$o$sha$d$sha$u$sha$l$sha$e$sha$M$sha$e$sha$m$sha$b$sha$e$sha$r$sha$ $sha$I$sha$n$sha$v$sha$o$sha$k$sha$e$sha$-$sha$P$sha$e$sha$s$sha$t$sha$e$sha$r$sha$,$sha$ $sha$N$sha$e$sha$w$sha$-$sha$F$sha$i$sha$x$sha$t$sha$u$sha$r$sha$e$sha$
+# Pester
+# Version: $version$
+# Changeset: 
+
+Resolve-Path $PSScriptRoot\Functions\*.ps1 | 
+    ? { -not ($_.ProviderPath.Contains(".Tests.")) } |
+    % { . $_.ProviderPath }
+
+function Invoke-Pester {
+    param(
+        [Parameter(Position=0,Mandatory=0)]
+        [string]$relative_path = ".",
+        [Parameter(Position=1,Mandatory=0)]
+        [string]$testName = $null, 
+        [Parameter(Position=2,Mandatory=0)]
+        [switch]$EnableExit, 
+        [Parameter(Position=3,Mandatory=0)]
+        [string]$OutputXml = ''
+    )
+    Reset-GlobalTestResults
+    . "$PSScriptRoot\ObjectAdaptations\PesterFailure.ps1"
+    Update-TypeData -pre "$PSScriptRoot\ObjectAdaptations\types.ps1xml" -ErrorAction SilentlyContinue
+
+    $fixtures_path = Resolve-Path $relative_path
+    Write-Host "Executing all tests in $fixtures_path"
+
+    Get-ChildItem $fixtures_path -Include "*.ps1" -Recurse |
+        ? { $_.Name -match "\.Tests\." } |
+        % { & $_.PSPath }
+
+    Write-TestReport
+
+    if($OutputXml) {
+        $Global:ModulePath = $PSScriptRoot
+        Write-NunitTestReport (Get-GlobalTestResults) $OutputXml 
+    }
+    if ($EnableExit) { Exit-WithCode }
+}
+
+function Write-UsageForCreateFixture {
+    "invalid usage, please specify (path, name)" | Write-Host
+    "eg: .\Create-Fixture -Path Foo -Name Bar" | Write-Host
+    "creates .\Foo\Bar.ps1 and .\Foo.Bar.Tests.ps1" | Write-Host
+}
+
+function Create-File($file_path, $contents = "") {
+
+    if (-not (Test-Path $file_path)) {
+        $contents | Out-File $file_path -Encoding ASCII
+        "Creating" | Write-Host -Fore Green -NoNewLine
+    } else {
+        "Skipping" | Write-Host -Fore Yellow -NoNewLine
+    }
+    " => $file_path" | Write-Host
+}
+
+function New-Fixture($path, $name) {
+
+    if ([String]::IsNullOrEmpty($path) -or [String]::IsNullOrEmpty($name)) {
+        Write-UsageForCreateFixture
+        return
+    }
+
+    # TODO clean up $path cleanup
+    $path = $path.TrimStart(".")
+    $path = $path.TrimStart("\")
+
+    if (-not (Test-Path $path)) {
+        & md $path | Out-Null
+    }
+
+    $test_code = "function $name {
+
+    }"
+
+    $fixture_code = "`$here = Split-Path -Parent `$MyInvocation.MyCommand.Path
+    `$sut = (Split-Path -Leaf `$MyInvocation.MyCommand.Path).Replace(`".Tests.`", `".`")
+    . `"`$here\`$sut`"
+
+    Describe `"$name`" {
+
+        It `"does something useful`" {
+            `$true.should.be(`$false)
+        }
+    }"
+
+    Create-File "$path\$name.ps1" $test_code
+    Create-File "$path\$name.Tests.ps1" $fixture_code
+}
+
+Export-ModuleMember Describe, Context, It, In
+Export-ModuleMember Invoke-Pester, New-Fixture
